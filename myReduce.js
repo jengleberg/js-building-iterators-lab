175910aec0c32cf80bb06351e6a33e7814e5e576
jengleberg <jeffengleberg@hotmail.com>
2017-09-19 08:40:42 -0600

    problem 3 incomplete

--- myReduce.js
+++ myReduce.js
@@ -1,9 +1,17 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
+function myReduce(arr, callback, initialValue) {
 
 //  CODE INSIDE HERE   //
+
+var newArr = [0];
+
+for(i=1;i<arr.length;i++) {
+	newArr = callback(newArr, arr[i], i, arr);
+}
+
+return newArr;
 
 }
 
